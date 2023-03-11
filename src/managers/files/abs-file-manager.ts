e4b83fdeeefafe0810fa5957e741796dd6dc2f9a
keshav <kdooleea@yahoo.ca>
2023-03-10 21:04:45 -0500

    fix: recursively finding .git and opened file matching paths to find remaining file path

--- src/managers/files/abs-file-manager.ts
+++ src/managers/files/abs-file-manager.ts
@@ -12,15 +12,33 @@ export abstract class AbsFileManager extends AbsManager {
 
   protected abstract getBranch(): Promise<string>;
 
+  // for 2 paths, given that the second path is longer that the first one, find recursively the common string from the starting position
+  private findCommonPath(path1: string[], path2: string[], currPath: string = ""): string {
+    // have split all elements from array 1 which is now empty
+    if (path1.length === 0) {
+      return currPath;
+    }
+
+    // found a match, move to the next position
+    if (path1[0].toLowerCase() === path2[0].toLowerCase()) {
+      path1.shift();
+      currPath += path2.shift() + "/";
+      return this.findCommonPath(path1, path2, currPath);
+    }
+
+    // current position is unmatch, return constructed path from beginning
+    return currPath;
+  }
+
   // concatonate working directory url with filePath
   private async openFile(url: string): Promise<void> {
     // extract repository name from Git url
     const repositoryName = StringUtils.getLastSubString(url);
 
     // from the OS full path, extract only the path that follows the repository name
-    const openedFile = this.process.getFileName();
+    const openedFilePath = this.process.getFileName();
 
-    if (!openedFile || !repositoryName) {
+    if (!openedFilePath || !repositoryName) {
       return VsCode.showMessage("No file is currently opened!");
     }
 
@@ -31,15 +49,20 @@ export abstract class AbsFileManager extends AbsManager {
       return VsCode.showMessage("Sorry, no git repository found!");
     }
 
-    const filePath = StringUtils.formatSlashes(openedFile).split(dotGitPath)[1];
+    // get match strings from .git file path and current file path
+    const path1 = dotGitPath.split("/");
+    const path2 = StringUtils.formatSlashes(openedFilePath).split("/");
+    const commonPath = this.findCommonPath(path1, path2);
 
-    if (!filePath) {
+    if (!commonPath) {
       return VsCode.showMessage("Sorry, this file can't be opened!");
     }
 
     // construct URL to file path
     const branch = await this.getBranch();
-    let fileURL = `${url}/blob/${branch}${filePath}`;
+    const filePath = StringUtils.formatSlashes(openedFilePath).split(commonPath)[1];
+
+    let fileURL = `${url}/blob/${branch}/${filePath}`;
     fileURL = StringUtils.formatSlashes(fileURL);
 
     const message = `Opened ${StringUtils.getLastSubString(fileURL)} on ${branch}`;
